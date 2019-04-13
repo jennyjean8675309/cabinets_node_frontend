f288aad6d5a6d9c2c3241b73a5d6c76b762537da
jennyjean <jennyjean8675309@yahoo.com>
2019-04-13 16:40:23 -0400

    Created my first component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Jenny's Vue.js App'
+      msg: "Welcome to Jenny's Vue.js App"
     }
   }
 }
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='home'>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      message: 'This is my home page.'
+    }
+  }
+}
+</script>
+
+<stye scoped>
+
+h1 {
+  font-family: 'Chalkduster';
+  color: pink;
+}
