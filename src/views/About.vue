9f5883360455e9a4a6506b1301f37a18db394a1e
jennyjean <jennyjean8675309@yahoo.com>
2019-04-20 20:03:46 -0400

    Added a route and a component for About

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id='about'>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      message: 'This is my about page.'
+    }
+  }
+}
+</script>
+
+<style>
+#about {
+  font-family: 'Arial';
+  color: green
+}
+</style>
